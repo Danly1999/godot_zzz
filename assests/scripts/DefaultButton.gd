a08bdea71eafc61d79f4a0f48c72abc1e702ffa6
Danly1999 <1594975543@qq.com>
2024-09-29 01:28:28 +0800

    1

--- assests/scripts/DefaultButton.gd
+++ assests/scripts/DefaultButton.gd
@@ -1,4 +1,4 @@
-extends Control
+extends BaseButton
 
 var tween
 # Called when the node enters the scene tree for the first time.
@@ -8,21 +8,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-func _gui_input(event):
-	if event is InputEventMouseButton:
-		print(event)
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if event.pressed:
-				if tween:
-					tween.kill() # 终止之前的补间动画。
-				tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
-				tween.tween_property(self, "scale", Vector2(0.9, 0.9), 0.2) # 缩小
-			elif !event.pressed:
-				if tween:
-					tween.kill() # 终止之前的补间动画。
-				tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
-				tween.tween_property(self, "scale", Vector2(1, 1), 0.2) # 恢复原大小
 
 func _on_mouse_entered() -> void:
 	if tween:
@@ -40,4 +25,20 @@ func _on_mouse_exited() -> void:
 	tween = get_tree().create_tween().set_ease(Tween.EASE_OUT)
 	tween.tween_property(self, "scale", Vector2(1,1), 0.1)
 	print("exited")
+	pass # Replace with function body.
+
+
+func _on_button_down() -> void:
+	if tween:
+		tween.kill() # 终止之前的补间动画。
+	tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(self, "scale", Vector2(0.9, 0.9), 0.2) # 缩小
+	pass # Replace with function body.
+
+
+func _on_button_up() -> void:
+	if tween:
+		tween.kill() # 终止之前的补间动画。
+	tween = get_tree().create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.2) # 恢复原大小
 	pass # Replace with function body.
